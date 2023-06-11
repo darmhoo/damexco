e5524a7442536b24bce8e530cab6325144e2e8b1
Omodamola Oladeji <darmhoo@hotmail.com>
2023-06-11 20:26:48 +0000

    initial commit

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <div>
+        <header>
+            <h1>Damola Inertia</h1>
+        </header>
+        <main>
+            <slot />
+        </main>
+        <footer>Footer</footer>
+        
+    </div>
+</template>
+<script setup>
+
+</script>
+<style lang="">
+    
+</style>
---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Layout from '../Layouts/Layout.vue';
+</script>
+<template>
+   <Layout>
+    <section>
+      <h2>Read our latest articles</h2>
+      <article>
+        <div>
+          <img src="/images/kinsta-logo.png" alt="Article thumbnail" />
+        </div>
+
+        <h3>Title for the blog</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum rem
+          itaque error vel perferendis aliquam numquam dignissimos, expedita
+          perspiciatis consectetur!
+        </p>
+
+        <a href="#">Read more</a>
+      </article>
+    </section>
+   </Layout>
+</template>
