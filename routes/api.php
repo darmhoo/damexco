a7a4e657e8afb14a21abc4bc4056ee9e071b1646
Omodamola Oladeji <darmhoo@hotmail.com>
2023-09-07 17:37:45 +0000

    change url

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/get-profile', [UserProfileController::class, 'getProfile']);
+Route::get('/get_profile', [UserProfileController::class, 'getProfile']);
